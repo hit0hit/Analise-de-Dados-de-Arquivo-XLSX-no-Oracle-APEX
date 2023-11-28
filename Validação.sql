d69d4356bc15d30b196a37120569b621f84af7d9
Dev_Hit <89605089+hit0hit@users.noreply.github.com>
2023-11-28 11:44:44 -0300

    Create Validação.sql

---
+++ Validação.sql
@@ -0,0 +1,8 @@
+--Expressão
+lower( :P19_FILE ) like '%.xlsx' or
+lower( :P19_FILE ) like '%.xml' or
+lower( :P19_FILE ) like '%.csv' or
+lower( :P19_FILE ) like '%.txt' or
+lower( :P19_FILE ) like '%.js' or
+lower( :P19_FILE ) like '%.json' or
+lower( :P19_FILE ) like '%.geojson'
